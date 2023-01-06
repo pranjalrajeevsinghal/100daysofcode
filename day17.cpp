f22b6ea48da55b6def0737f537d158ec1abb9388
pranjalrajeevsinghal <84920940+pranjalrajeevsinghal@users.noreply.github.com>
2023-01-06 20:33:57 +0530

    Create day17.cpp

---
+++ day17.cpp
@@ -0,0 +1,30 @@
+//Qlink-> https://leetcode.com/problems/count-and-say/description/
+//Code-
+
+string countAndSay(int n) {
+        if(n==1)
+        {
+            return "1";
+        }
+        string temp = countAndSay(n-1);
+        string res = "";
+        int count = 1;
+        for(int i=1;i<temp.length();i++)
+        {
+            if(temp[i-1] == temp[i])
+            {
+                count++;
+            }
+            else
+            {
+                res.push_back(count + '0');
+                res.push_back(temp[i-1]);
+                count = 1;
+            }
+        }
+        
+        res.push_back(count+'0');
+        res.push_back(temp[temp.size()-1]);
+
+        return res;
+    }
