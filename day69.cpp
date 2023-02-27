ef65e04dccae81b6072e30864e352b740932a530
pranjalrajeevsinghal <84920940+pranjalrajeevsinghal@users.noreply.github.com>
2023-02-27 20:45:15 +0530

    Create day69.cpp

---
+++ day69.cpp
@@ -0,0 +1,25 @@
+//Qlink-> https://leetcode.com/problems/remove-linked-list-elements/
+//Code-
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        while(head && head->val == val)
+        {
+            head = head->next;
+        }
+        ListNode* cur = head;
+        while(cur)
+        {
+            if(cur->next && cur->next->val == val)
+            {
+                cur->next = cur->next->next;
+            }
+            else
+            {
+                cur = cur->next;
+            } 
+        }
+        return head;
+    }
+
+};
